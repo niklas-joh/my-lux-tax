96c9a36005debeebffdb213ae4935e6fd4dce204
Niklas Johansson <niklas@hjuvik.net>
2020-11-13 20:29:05 +0100

    Feat: add username path to prepare for HTML

--- hello-world.py
+++ hello-world.py
@@ -2,5 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def hello():
+    return 'Hello, World'
+
+@app.route('/user/<user_name>')
+def user_name(user_name): 
+    return "Hello " + user_name + "!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
