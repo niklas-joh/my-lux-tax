76b3e9df8bbdcffb641ace67a082f5c3fd9359fc
Niklas Johansson <niklas@hjuvik.net>
2020-11-16 10:19:29 +0100

    Feat: Add HTML Templates

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return 'Hello, World'
+def home():
+    return render_template("home.html")
 
 @app.route('/user/<user_name>')
 def user_name(user_name): 
