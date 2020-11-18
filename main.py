a87cb4795ad7c1e41649bd0eee32ceeace0ccb16
Niklas Johansson <niklas@hjuvik.net>
2020-11-18 08:56:11 +0100

    Feat: Add about html page

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template("index.html")
 
 @app.route('/user/<user_name>')
 def user_name(user_name): 
     return "Hello " + user_name + "!"
+
+@app.route('/about')
+def about():
+   return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
