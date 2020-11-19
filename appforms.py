d184eb3c4e230403ed79312d3f63ed9795826a44
Niklas Johansson <niklas@hjuvik.net>
2020-11-19 08:03:43 +0100

    Feat: Add login form python page

---
+++ appforms.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+
+# Load secret key from file
+app.config.from_json('config.json')
+
+# Get secret keys from config.json
+app.secret_key = app.config['APP_SECRET_KEY']
+app.config['SESSION_TYPE'] = 'filesystem'
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('formlogin.html', title='Sign In', form=form)
+
+if __name__ == '__main__':
+    app.run(debug=True)
